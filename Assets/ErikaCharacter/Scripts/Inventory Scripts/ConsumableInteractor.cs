8696470a3f0f7b74bf9a7c7b3f923add042bfd16
Sree Charan <35174179+txmaniac@users.noreply.github.com>
2019-11-08 15:49:25 +0530

    Connected the 3d object to inventory sprite

--- Assets/ErikaCharacter/Scripts/Inventory Scripts/ConsumableInteractor.cs
+++ Assets/ErikaCharacter/Scripts/Inventory Scripts/ConsumableInteractor.cs
@@ -1,6 +1,6 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 /*
   This class identifies the gameobject in 3d space as the consumables which are to be added to the inventory on pickup action
@@ -9,7 +9,8 @@ using System.Collections.Generic;
 */
 
 public class ConsumableInteractor{
-  public int consumableId;
+  public Consumable consumable;
+  public GameObject PlayerHand;
   
   void OnTriggerEnter(Collider other){
     if(other.tag == "Player"){
@@ -17,7 +18,35 @@ public class ConsumableInteractor{
       if(Input.GetKeyDown(KeyCode.E)){
         // make the object child of the player and copy the transform according to the position which is default to the gameObject.
         // also add the object item in the list of the Inventory.
+        transform.parent = PlayerHand.transform;
+        FindInList(consumable)
       }
+      
+      else{
+        // show a message on the screen regarding the object and the show the message "Press E to Interact"
+      }
+    }
+  }
+  
+  public void FindInList(Consumable consumable){
+    bool findFlag = false;
+    int findIndex;
+    for(int i=0; i<ConsumableInventoryManager.instance.list.Count; i++){
+      // this condition checks if the object already exists in the inventory
+      if(consumable.id == ConsumableInventoryManager.instance.list[i].id){
+        findFlag = true;
+        findIndex = i;
+        break;
+      }
+    }
+    
+    // increment the counter of the item in the inventory if the item already exists in the inventory
+    // else add the item in the inventory
+    if(findFlag){
+      ConsumableInventoryManager.instance.list[findIndex].count++;
+    }
+    else{
+      ConsumableInventoryManager.instance.Add(consumable);
     }
   }
 }
